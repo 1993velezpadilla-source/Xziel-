7e8683498961368b4629d9f2a11bbc9e42f064d6
1993velezpadilla-source <1993velezpadilla@gmail.com>
2026-09-20 00:34:16 -0400

    Install QuakeC generator dependencies

--- scripts/prepare_android.sh
+++ scripts/prepare_android.sh
@@ -34,6 +34,7 @@ python3 "$ROOT/scripts/patch_vril_android.py" "$DEPS/vril"
 python3 "$ROOT/scripts/patch_vril_weaponhud.py" "$DEPS/vril"
 
 echo "==> Patching and compiling Xziel mobile QuakeC"
+python3 -m pip install --quiet pandas fastcrc
 python3 "$ROOT/scripts/patch_quakec_mobile.py" "$DEPS/quakec"
 chmod +x "$DEPS/quakec/bin/fteqcc-cli-lin" "$DEPS/quakec/tools/qc-compiler-gnu.sh"
 (
