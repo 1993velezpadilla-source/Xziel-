fe98151c71c260134443c7148ba5bcc6991002d9
1993velezpadilla-source <1993velezpadilla@gmail.com>
2026-09-21 03:46:48 -0400

    Enable v0.23 mobile HUD pass

--- scripts/prepare_android.sh
+++ scripts/prepare_android.sh
@@ -40,6 +40,7 @@ python3 "$ROOT/scripts/patch_vril_camera_feel.py" "$DEPS/vril"
 python3 "$ROOT/scripts/patch_vril_animation_feel.py" "$DEPS/vril"
 python3 "$ROOT/scripts/patch_vril_mobile_v021.py" "$DEPS/vril"
 python3 "$ROOT/scripts/patch_vril_mobile_v022.py" "$DEPS/vril"
+python3 "$ROOT/scripts/patch_vril_mobile_v023.py" "$DEPS/vril"
 python3 "$ROOT/scripts/patch_vril_nacht_enhanced.py" "$DEPS/vril"
 python3 "$ROOT/scripts/patch_vril_nacht_textures.py" "$DEPS/vril"
 
