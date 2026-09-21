209643d3aa7373fb98102528b48a4c9971ed939f
1993velezpadilla-source <1993velezpadilla@gmail.com>
2026-09-21 14:08:46 -0400

    engine: add Xz CPU stage timing API

--- engine/xz/xz_android_runtime.h
+++ engine/xz/xz_android_runtime.h
@@ -7,9 +7,18 @@
 extern "C" {
 #endif
 
+typedef enum {
+    XZ_CPU_STAGE_UPDATE = 0,
+    XZ_CPU_STAGE_RENDER,
+    XZ_CPU_STAGE_AUDIO,
+    XZ_CPU_STAGE_COUNT
+} XzCpuStage;
+
 void XzAndroidRuntime_Init(size_t engine_heap_bytes);
 void XzAndroidRuntime_BeginFrame(double now_seconds);
 void XzAndroidRuntime_EndFrame(double now_seconds);
+void XzAndroidRuntime_BeginStage(XzCpuStage stage, double now_seconds);
+void XzAndroidRuntime_EndStage(XzCpuStage stage, double now_seconds);
 void XzAndroidRuntime_Shutdown(void);
 
 #ifdef __cplusplus
