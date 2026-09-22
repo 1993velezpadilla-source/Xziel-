a9be5025950be798a55de1afbf8e343c0d8c15b5
1993velezpadilla-source <1993velezpadilla@gmail.com>
2026-09-22 00:19:00 -0400

    CI: force alias shadows during parity probe

--- android/NZPActivity.java
+++ android/NZPActivity.java
@@ -148,7 +148,8 @@ public class NZPActivity extends SDLActivity {
                 "-basedir", dataRoot.getAbsolutePath(),
                 "+map", ciMap,
                 "+fog", "96", "768", "16", "20", "24",
-                "++attack"
+                "++attack",
+                "++r_shadows", "1"
             };
         }
 
