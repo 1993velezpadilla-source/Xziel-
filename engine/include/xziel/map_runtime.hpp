8bdc4fcdebb559ba9b19015841a9548a8d76dfc7
1993velezpadilla-source <1993velezpadilla@gmail.com>
2026-09-22 09:15:17 -0400

    engine: extend native maps with Sanctum spawn and arena data

--- engine/include/xziel/map_runtime.hpp
+++ engine/include/xziel/map_runtime.hpp
@@ -12,8 +12,9 @@ namespace xziel {
 
 inline constexpr std::size_t kMaxMapBoxes = 128;
 inline constexpr std::size_t kMaxMapDoors = 16;
-inline constexpr std::size_t kMaxMapWindows = 8;
+inline constexpr std::size_t kMaxMapWindows = 32;
 inline constexpr std::size_t kMaxMapGenericInteractions = 16;
+inline constexpr std::size_t kMaxMapZombieSpawns = 32;
 
 struct MapBoxDefinition {
     std::uint32_t id = 0;
@@ -39,6 +40,19 @@ struct MapDefinition {
     std::array<MapBoxDefinition, kMaxMapBoxes> boxes{};
     std::size_t boxCount = 0;
 
+    Vec3 playerSpawnFeet{};
+    float playerSpawnYawDegrees = 0.0f;
+    bool hasPlayerSpawn = false;
+
+    std::array<Vec3, kMaxMapZombieSpawns> zombieSpawns{};
+    std::size_t zombieSpawnCount = 0;
+
+    float arenaMinimumX = 0.0f;
+    float arenaMaximumX = 0.0f;
+    float arenaMinimumZ = 0.0f;
+    float arenaMaximumZ = 0.0f;
+    bool hasArenaBounds = false;
+
     std::array<MapDoorEntity, kMaxMapDoors> doors{};
     std::size_t doorCount = 0;
 
@@ -58,6 +72,9 @@ struct MapLoadResult {
     std::size_t doors = 0;
     std::size_t windows = 0;
     std::size_t interactions = 0;
+    std::size_t zombieSpawns = 0;
+    bool playerSpawnApplied = false;
+    bool arenaBoundsApplied = false;
 };
 
 class MapRuntime final {
