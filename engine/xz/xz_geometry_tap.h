379d8c3e05f6e34c68e844097f91f01387ff83e0
1993velezpadilla-source <1993velezpadilla@gmail.com>
2026-09-21 23:24:53 -0400

    Xz: classify sky and water as special geometry

--- engine/xz/xz_geometry_tap.h
+++ engine/xz/xz_geometry_tap.h
@@ -15,8 +15,15 @@ typedef enum {
     XZ_GEOMETRY_ALIAS = 0,
     XZ_GEOMETRY_SURFACE,
     XZ_GEOMETRY_SPRITE,
-    XZ_GEOMETRY_EFFECT
+    XZ_GEOMETRY_EFFECT,
+    XZ_GEOMETRY_SPECIAL
 } XzGeometryKind;
+
+typedef enum {
+    XZ_GEOMETRY_SPECIAL_NONE = 0,
+    XZ_GEOMETRY_SPECIAL_SKY,
+    XZ_GEOMETRY_SPECIAL_WATER
+} XzGeometrySpecialKind;
 
 typedef enum {
     XZ_GEOMETRY_TRIANGLES = 0,
@@ -66,6 +73,7 @@ typedef struct {
     unsigned int first_index;
     unsigned int index_count;
     XzGeometryKind kind;
+    XzGeometrySpecialKind special_kind;
     int texture_id;
     XzGeometryRenderState state;
     float modelview[16];
@@ -86,6 +94,9 @@ typedef struct {
     unsigned int surface_batches;
     unsigned int sprite_batches;
     unsigned int effect_batches;
+    unsigned int special_batches;
+    unsigned int sky_batches;
+    unsigned int water_batches;
 
     unsigned int dropped_batches;
     unsigned int dropped_vertices;
@@ -136,6 +147,18 @@ int XzGeometryTap_CapturePrimitive(
     unsigned int texture_offset,
     XzGeometryPrimitive primitive,
     int texture_id,
+    const XzGeometryRenderState *state,
+    const float modelview[16],
+    const float projection[16]);
+
+int XzGeometryTap_CaptureSpecialFan(
+    const float *source,
+    unsigned int count,
+    unsigned int stride_floats,
+    unsigned int position_offset,
+    unsigned int texture_offset,
+    int texture_id,
+    XzGeometrySpecialKind special_kind,
     const XzGeometryRenderState *state,
     const float modelview[16],
     const float projection[16]);
