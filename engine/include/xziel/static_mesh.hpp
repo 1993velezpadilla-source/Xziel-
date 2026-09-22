2908211f62b992200fc244e49e5c8f1bfdff65b1
1993velezpadilla-source <1993velezpadilla@gmail.com>
2026-09-22 01:38:01 -0400

    engine: lift legacy static-mesh batch ceiling for native worlds

--- engine/include/xziel/static_mesh.hpp
+++ engine/include/xziel/static_mesh.hpp
@@ -10,7 +10,7 @@
 namespace xziel {
 
 inline constexpr std::uint32_t kStaticMeshFormatVersion = 2U;
-inline constexpr std::size_t kMaxStaticMeshBatches = 512U;
+inline constexpr std::size_t kMaxStaticMeshBatches = 2048U;
 inline constexpr std::uint32_t kMaxStaticMeshVertices = 8000000U;
 inline constexpr std::uint32_t kMaxStaticMeshIndices = 12000000U;
 
