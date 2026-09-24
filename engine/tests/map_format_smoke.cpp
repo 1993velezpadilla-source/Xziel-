5c2c088a1cdeec8290bba24db51035e61b90852b
1993velezpadilla-source <1993velezpadilla@gmail.com>
2026-09-24 10:15:15 -0400

    tests: cover XMAP v3 authored lights

--- engine/tests/map_format_smoke.cpp
+++ engine/tests/map_format_smoke.cpp
@@ -63,8 +63,30 @@ box 10 0 -1.70 0 30 0.10 25 0 0 1 1
     assert(map.zombieSpawnCount == 2);
     assert(map.zombieSpawns[1].x == 14.0f);
 
-    constexpr std::string_view invalid = R"MAP(
+    constexpr std::string_view lightingMap = R"MAP(
 xziel_map 3
+player_spawn 0 -1.58 0 0
+arena -12 12 -14 14
+light 9001 point 0 2.5 1 0 -1 0 1 0.35 0.08 7.5 8.0 24 42 1.25 0.25 1.8 1 1 1
+light 9002 spot 3 3 -4 -0.2 -0.8 0.4 0.16 0.28 0.9 9.0 12.0 18 36 1.0 0.0 0.0 1 1 1
+)MAP";
+
+    assert(
+        xziel::parseMapText(
+            lightingMap,
+            map,
+            error));
+    assert(map.lightCount == 2U);
+    assert(map.lights[0].id == 9001U);
+    assert(map.lights[0].type == xziel::MapLightType::Point);
+    assert(map.lights[0].castsShadows);
+    assert(map.lights[0].volumetric);
+    assert(map.lights[1].type == xziel::MapLightType::Spot);
+    assert(map.lights[1].innerConeDegrees == 18.0f);
+    assert(map.lights[1].outerConeDegrees == 36.0f);
+
+    constexpr std::string_view invalid = R"MAP(
+xziel_map 4
 )MAP";
 
     assert(
