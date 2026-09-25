239f7e8390bae4a6d58cbd7495f6e30f1b4b9d3f
1993velezpadilla-source <1993velezpadilla@gmail.com>
2026-09-25 01:12:05 -0400

    weapon: add first-person viewmodel profiles

--- engine/include/xziel/weapon_catalog.hpp
+++ engine/include/xziel/weapon_catalog.hpp
@@ -16,11 +16,69 @@ enum class WeaponArchetype : std::uint8_t {
     SniperRifle,
 };
 
+enum class WeaponViewmodelAsset : std::uint8_t {
+    Sidearm,
+    SubmachineGun,
+    AssaultRifle,
+    MarksmanRifle,
+    Shotgun,
+    LightMachineGun,
+    SniperRifle,
+};
+
+struct WeaponViewmodelPose {
+    float x = 0.265f;
+    float y = -0.205f;
+    float z = 0.220f;
+
+    float scale = 0.64f;
+
+    float yawRadians = -0.055f;
+    float pitchRadians = 0.018f;
+    float rollRadians = 0.030f;
+};
+
+struct WeaponViewmodelProfile {
+    WeaponViewmodelAsset asset =
+        WeaponViewmodelAsset::AssaultRifle;
+
+    // Calibrated in canonical XZIEL viewmodel space:
+    // +X right, +Y up, +Z stock-to-muzzle.
+    WeaponViewmodelPose hip{};
+    WeaponViewmodelPose ads{
+        -0.008f,
+        -0.065f,
+        0.160f,
+        0.58f,
+        0.0f,
+        0.018f,
+        0.0f,
+    };
+
+    float verticalFovDegrees = 72.0f;
+
+    float loweringX = 0.030f;
+    float loweringY = -0.20f;
+    float loweringZ = 0.030f;
+
+    float reloadY = -0.070f;
+    float reloadZ = 0.024f;
+    float reloadYawRadians = 0.045f;
+    float reloadPitchRadians = -0.090f;
+    float reloadRollRadians = -0.220f;
+
+    float fireY = 0.016f;
+    float fireZ = -0.055f;
+    float firePitchRadians = 0.020f;
+    float fireRollRadians = 0.020f;
+};
+
 struct WeaponProfile {
     WeaponArchetype archetype =
         WeaponArchetype::Sidearm;
 
     WeaponConfig controller{};
+    WeaponViewmodelProfile viewmodel{};
 
     float baseDamage = 34.0f;
     float falloffStartMeters = 12.0f;
